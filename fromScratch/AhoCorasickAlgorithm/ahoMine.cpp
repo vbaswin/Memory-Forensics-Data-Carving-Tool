1c87a1b147f14c9a86c0759a03d4f265d92c21b2
vbaswin <aswinvb10a@gmail.com>
2024-04-03 17:01:32 +0530

    tree creted, but need to check using bfs

--- fromScratch/AhoCorasickAlgorithm/ahoMine.cpp
+++ fromScratch/AhoCorasickAlgorithm/ahoMine.cpp
@@ -6,45 +6,72 @@ using namespace std;
 class node {
 	char val_;
 	bool endNode_;
+	bool isRoot_;
 	class node *failureLink_;
-	// vector<class node *> nextNodes_;
-	class node *nextNodes_;
+	vector<class node *> nextNodes_;
+	int noOfChild_;
+	// class node *nextNodes_;
 public:
-	node(char val, bool endNode, node *failureLink) : val_(val), endNode_(endNode), failureLink_(failureLink), nextNodes_(NULL) {
-		// cout << "inside node constructor\n";
+	node(char val, bool endNode, bool isRoot, node *failureLink) : val_(val), endNode_(endNode), isRoot_(isRoot), failureLink_(failureLink), noOfChild_(0) {
+		nextNodes_.reserve(1);
 	}
 
-	node *getNextNode() {
-		return nextNodes_;
+	node *getNextNode(int idx) {
+		return nextNodes_[idx];
 	}
 
-	char getNodeValue() {
+	char getValue() {
 		return val_;
 	}
 
+	node *getChild(int idx) {
+		return nextNodes_[idx];
+	}
+
 	void setNextNode(node *nextNode) {
-		nextNodes_ = nextNode;
+		nextNodes_[noOfChild_++] = nextNode;
+	}
+
+	void addChildNode(node *child) {
+		nextNodes_[noOfChild_++] = child;
+	}
+
+	int getNoOfChildNodes() {
+		return noOfChild_;
 	}
 };
 
 void displayTree(node *head) {
-	for (node *temp = head; temp != NULL; temp = temp->getNextNode())
-		cout << temp->getNodeValue() << " -> ";
+	// for (node *temp = head; temp != NULL; temp = temp->getNextNode(0))
+	// cout << temp->getValue() << " -> ";
+
+	cout << head->getNextNode(0)->getValue() << "\n";
+	cout << head->getNextNode(0)->getNextNode(0)->getValue() << "\n";
+	cout << head->getNextNode(1)->getValue() << "\n";
+	cout << head->getNextNode(1)->getNextNode(0)->getValue() << "\n";
 }
 
 int main() {
-	node *head = NULL, *prevNode = NULL, *newNode = NULL;
+	node *head = new node('R', false, true, NULL), *prevNode = NULL, *newNode = NULL, *child = NULL;
 
-	vector<string> sig = {"abcde"};
-
-	for (auto &val : sig[0]) {
-		if (head == NULL) {
-			head = new node(val, false, NULL);
-			prevNode = head;
-		} else {
-			newNode = new node(val, false, NULL);
-			prevNode->setNextNode(newNode);
-			prevNode = newNode;
+	vector<string> sigs = {"abcde", "123"};
+	for (auto &sig : sigs) {
+		prevNode = head;
+		for (auto &val : sig) {
+			int present = false;
+			for (int i = 0; i < prevNode->getNoOfChildNodes(); ++i) {
+				child = prevNode->getChild(i);
+				if (child->getValue() == val) {
+					prevNode = child;
+					present = true;
+					break;
+				}
+			}
+			if (!present) {
+				newNode = new node(val, false, false, NULL);
+				prevNode->setNextNode(newNode);
+				prevNode = newNode;
+			}
 		}
 	}
 
