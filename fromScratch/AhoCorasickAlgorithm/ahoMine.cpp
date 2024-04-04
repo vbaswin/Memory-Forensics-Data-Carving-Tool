341c275bf76868a2451dcec6455ae44ddafad7d1
vbaswin <aswinvb10a@gmail.com>
2024-04-04 10:47:54 +0530

    some how, tree is getting created ig, more cases to consider

--- fromScratch/AhoCorasickAlgorithm/ahoMine.cpp
+++ fromScratch/AhoCorasickAlgorithm/ahoMine.cpp
@@ -8,13 +8,18 @@ class node {
 	char val_;
 	bool endNode_;
 	bool isRoot_;
+	string pattern_;
 	class node *failureLink_;
 	vector<class node *> nextNodes_;
 	int noOfChild_;
 	// class node *nextNodes_;
 public:
-	node(char val, bool endNode, bool isRoot, node *failureLink) : val_(val), endNode_(endNode), isRoot_(isRoot), failureLink_(failureLink), noOfChild_(0) {
+	node(char val, bool endNode, bool isRoot, node *failureLink) : val_(val), endNode_(endNode), isRoot_(isRoot), pattern_(""), failureLink_(failureLink), noOfChild_(0) {
 		nextNodes_.reserve(1);
+	}
+
+	bool checkRoot() {
+		return isRoot_;
 	}
 
 	node *getNextNode(int idx) {
@@ -40,10 +45,31 @@ public:
 	int getNoOfChildNodes() {
 		return noOfChild_;
 	}
+
+	void setEndNode(string str) {
+		endNode_ = true;
+		pattern_ = str;
+	}
+
+	bool isEnd() {
+		return endNode_;
+	}
+
+	string getPattern() {
+		return pattern_;
+	}
 };
 
-void displayTree(node *head) {
-	queue<node *> q;
+void displayTree(node *head, bool isRootChild) {
+	if (isRootChild)
+		cout << "\n";
+	cout << head->getValue() << " -> ";
+	if (head->isEnd())
+		cout << "\nSig: " << head->getPattern() << "\n";
+	for (int i = 0; i < head->getNoOfChildNodes(); ++i)
+		displayTree(head->getChild(i), head->checkRoot());
+
+	// queue<node *> q;
 	// for (node *temp = head; temp != NULL; temp = temp->getNextNode(0))
 	// cout << temp->getValue() << " -> ";
 
@@ -52,43 +78,48 @@ void displayTree(node *head) {
 	// cout << head->getNextNode(1)->getValue() << "\n";
 	// cout << head->getNextNode(1)->getNextNode(0)->getValue() << "\n";
 
-	q.push(head);
-	int level = 0;
-	while (!q.empty()) {
-		node *curNode = q.front();
-		cout << curNode->getValue() << " ";
-		for (int i = 0; i < curNode->getNoOfChildNodes(); ++i) {
-			q.push(curNode->getChild(i));
-		}
-		q.pop();
-	}
+	// q.push(head);
+	// // int level = 0;
+	// while (!q.empty()) {
+	// 	node *curNode = q.front();
+	// 	cout << curNode->getValue() << " ";
+	// 	for (int i = 0; i < curNode->getNoOfChildNodes(); ++i) {
+	// 		q.push(curNode->getChild(i));
+	// 	}
+	// 	q.pop();
+	// }
 }
 
 int main() {
 	node *head = new node('R', false, true, NULL), *prevNode = NULL, *newNode = NULL, *child = NULL;
 
-	vector<string> sigs = {"abcde", "123"};
+	vector<string> sigs = {"ab", "abc", "ac"};
 	for (auto &sig : sigs) {
 		prevNode = head;
-		for (auto &val : sig) {
+		// cout << "Sig: " << sig << "\n";
+		for (int i = 0; i < (int)sig.size(); ++i) {
 			int present = false;
-			for (int i = 0; i < prevNode->getNoOfChildNodes(); ++i) {
-				child = prevNode->getChild(i);
-				if (child->getValue() == val) {
+			int noOfChild = prevNode->getNoOfChildNodes();
+			for (int j = 0; j < noOfChild; ++j) {
+				child = prevNode->getChild(j);
+				// cout << "Child value: " << child->getValue() << " Sig value: " << sig[i] << "\n";
+				if (child->getValue() == sig[i]) {
+					// cout << "present: " << sig[i] << "\n";
 					prevNode = child;
 					present = true;
 					break;
 				}
 			}
 			if (!present) {
-				newNode = new node(val, false, false, NULL);
+				newNode = new node(sig[i], false, false, NULL);
 				prevNode->setNextNode(newNode);
 				prevNode = newNode;
 			}
 		}
+		prevNode->setEndNode(sig);
 	}
 
-	displayTree(head);
+	displayTree(head, false);
 
 	return 0;
 }
