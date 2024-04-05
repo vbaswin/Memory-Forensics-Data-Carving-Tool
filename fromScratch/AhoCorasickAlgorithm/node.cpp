3f6e326565b69d6d83ada97c912b46ba13a14166
vbaswin <aswinvb10a@gmail.com>
2024-04-05 23:44:40 +0530

    separated into new node.cpp file

---
+++ fromScratch/AhoCorasickAlgorithm/node.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class node {
+	char val_;
+	bool endNode_;
+	bool isRoot_;
+	vector<char> pattern_;
+	class node *failureLink_;
+	vector<class node *> nextNodes_;
+	int noOfChild_;
+	// class node *nextNodes_;
+public:
+	node(char val, bool endNode, bool isRoot, node *failureLink) : val_(val), endNode_(endNode), isRoot_(isRoot), failureLink_(failureLink), noOfChild_(0) {
+		nextNodes_.reserve(1);
+		pattern_.reserve(1);
+	}
+
+	bool checkRoot() {
+		return isRoot_;
+	}
+
+	node *getNextNode(int idx) {
+		return nextNodes_[idx];
+	}
+
+	char getValue() {
+		return val_;
+	}
+
+	node *getChild(int idx) {
+		return nextNodes_[idx];
+	}
+
+	void setNextNode(node *nextNode) {
+		nextNodes_[noOfChild_++] = nextNode;
+	}
+
+	void addChildNode(node *child) {
+		nextNodes_[noOfChild_++] = child;
+	}
+
+	int getNoOfChildNodes() {
+		return noOfChild_;
+	}
+
+	void setEndNode(vector<char> v) {
+		endNode_ = true;
+		pattern_ = v;
+	}
+
+	bool isEnd() {
+		return endNode_;
+	}
+
+	vector<char> getPattern() {
+		return pattern_;
+	}
+};
